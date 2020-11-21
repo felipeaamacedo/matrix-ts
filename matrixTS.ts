80f2d06f4893d9bf19b5c1dc9bb5edbccbcdcc60
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-11-21 14:18:44 +0000

    function multiply() working property

--- matrixTS.ts
+++ matrixTS.ts
@@ -16,15 +16,17 @@ function multiply(MatA:number[][], MatB:number[][]):number[][]{
 	}
 
 
-	let MatC:number[][]=[[],[]]
+	//Problem with the creation of MATC, need to create a matrix with the correct size of the answer.
+	let MatC:Array<Array<number>>= new Array()
+		for(let i = 1; i<=MatA_Row;i++){
+			MatC.push(Array(MatB_Col))
+		}
+
 
 	for(let i=0; i<MatA_Row ;i++){
-		console.log(i)
 		for(let j=0; j<MatB_Col; j++){
-			console.log(j)
 			let sum:number = 0
 			for(let k=0; k<MatB_Row; k++){
-				console.log(k)
 				sum = sum + MatA[i][k]*MatB[k][j]
 			}
 			MatC[i][j]= sum	
@@ -35,10 +37,17 @@ function multiply(MatA:number[][], MatB:number[][]):number[][]{
 
 let A:number[][]= [[3, 4, 2]]
 let B:number[][]= [[13, 9, 7, 15], [8, 7, 4, 6], [6, 4, 0, 3]]
-let D:number[][]= [[1,3,2], [1,2,3]]
+let D:number[][]= [[2,1,4], [0,1,1]]
+let E:number[][]= [[6, 3, -1, 0], [1, 1, 0, 4], [-2, 5, 0, 2]]
 let C:number[][]= multiply(A,B)
+let F:number[][]= multiply(D,E)
+
+let G:number = F[0][0]
 
 console.log(A)
 console.log(size(A))
 console.log(C)
 console.log(size(C))
+console.log(F)
+console.log(size(F))
+console.log(G)
