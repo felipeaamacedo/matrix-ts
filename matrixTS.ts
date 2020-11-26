ce2e142a8bc1acdc576a1edde63a57c40db13917
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-11-26 17:20:38 +0000

    Addition of class Matrix and determinant method

--- matrixTS.ts
+++ matrixTS.ts
@@ -1,15 +1,76 @@
-//class Matrix{
-	//constructor(public row:number, public col:number){
-		//row= this.row
-		//col= this.col
-	//}
-		
-	//public	matrix:Array<Array<number>> = new Array()
-		//for(i = 1; i<=this.row;i++){
-			//this.matrix.push(Array(this.col))
-		//}
+class Matrix{
+    public data:number[][] = new Array()
 
-//}
+    constructor(private _row:number, private _col:number){
+        let row = this._row
+        let col = this._col
+
+        
+        //CREATING THE MATRIX
+        for(let i=0; i<this._row; i++){
+            this.data.push(Array(this._col))        
+        }
+    }
+
+    get row(){
+    	return this._row
+    }
+
+    get col(){
+    	return this._col
+    }
+
+    determinant():number{
+        if(this.row !== this._col){
+            throw new Error('The matrix is not a square')
+        }else{
+            if(this.row == 2){
+                return this.data[0][0]*this.data[1][1] - this.data[0][1]*this.data[1][0]
+            }else{
+                let iCof = 1
+                let sum:number = 0
+                for(let j = 0; j<this._col; j++){
+                    sum = sum + this.data[iCof-1][j]*(-1)**(iCof+j+1)*this.sub_matrix(iCof,j+1).determinant()
+                }
+                return sum
+            }
+        }
+    }
+
+    sub_matrix(del_row:number, del_col:number):Matrix{
+	    del_col = del_col-1
+	    del_row = del_row-1
+
+		let subMatrix:Matrix = new Matrix(this.row-1, this._col-1)
+		let flagRow:boolean = false
+		let flagCol:boolean = false
+
+		for(let j = 0; j<this._col; j++){
+			flagRow = false
+			if((j == del_col) && (del_col < this._col)){
+				j++	
+				flagCol=true
+			}
+			for(let i = 0; i<this.row; i++){
+				if((i == del_row ) && (del_row < this.row)){
+					i++		
+					flagRow=true
+				}
+				if ((flagCol==true)&&(flagRow==false)){
+					subMatrix.data[i][j-1] = this.data[i][j]
+				}else if((flagCol==false)&&(flagRow==true)){
+					subMatrix.data[i-1][j] = this.data[i][j]
+				}else if((flagCol==true)&&(flagRow==true)){
+					subMatrix.data[i-1][j-1] = this.data[i][j]
+				}else{
+					subMatrix.data[i][j] = this.data[i][j]
+				}
+			}
+		}
+	    return subMatrix
+	}
+
+}
 
 
 
@@ -47,97 +108,14 @@ function multiply(MatA:number[][], MatB:number[][]):number[][]{
 	return MatC
 }
 
-function transpose(M:number[][]):number[][]{
-	let M_row:number = size(M)[0]
-	let M_col:number = size(M)[1]	
-
-	let Mtransp:Array<Array<number>>= new Array()
-	for(let i = 1; i<=M_col;i++){
-		Mtransp.push(Array(M_row))
-	}
-	
-	for(let i=0; i<M_row;i++){
-		for(let j=0;j<M_col;j++){
-			Mtransp[j][i] = M[i][j]			
+function transpose(M:Matrix):Matrix{
+	let Mtransp:Matrix= new Matrix(M.row, M.col)
+	for(let i=0; i<M.row;i++){
+		for(let j=0;j<M.col;j++){
+			Mtransp.data[j][i] = M.data[i][j]			
 		}
 	}
 
 	return Mtransp
 }
 
-function minor(M:number[][], del_row:number, del_col:number):number[][]{
-	let M_row:number = size(M)[0]
-	let M_col:number = size(M)[1]	
-	let Minor_Matrix:Array<Array<number>> = new Array()
-	let flagRow:boolean = false
-	let flagCol:boolean = false
-
-
-	for(let i = 1; i<=M_col-1;i++){
-		Minor_Matrix.push(Array(M_row-1))
-	}
-
-	for(let j = 0; j<M_col; j++){
-		flagRow = false
-		if((j == del_col-1) && (del_col <M_col)){
-			j++	
-			flagCol=true
-		}
-		for(let i = 0; i<M_row; i++){
-			if((i == del_row-1 )&&(del_row <M_row)){
-				i++		
-				flagRow=true
-			}
-			if ((flagCol==true)&&(flagRow==false)){
-				Minor_Matrix[i][j-1] = M[i][j]
-			}else if((flagCol==false)&&(flagRow==true)){
-				Minor_Matrix[i-1][j] = M[i][j]
-			}else if((flagCol==true)&&(flagRow==true)){
-				Minor_Matrix[i-1][j-1] = M[i][j]
-			}else{
-				Minor_Matrix[i][j] = M[i][j]
-			}
-		}
-	}
-
-	return Minor_Matrix 
-}
-
-function determinant(M:number[][]):number{
-	let M_row:number = size(M)[0]
-	let M_col:number = size(M)[1]	
-	
-	if(M_row==M_col && M_col+M_row>=4){
-		for(let j = 0; j<=M_row; j++){
-			for(let i = 0; i<=M_row; i++){
-				// STILL NEEDS TO ADD THE MINOR OF IJ
-				//sum = sum + M[i][j]*(-1)**(i+j)*
-			}
-		}	
-	}	
-
-	let minor = 0
-	return minor
-
-}
-
-let A:number[][]= [[3, 4, 2]]
-let B:number[][]= [[13, 9, 7, 15], [8, 7, 4, 6], [6, 4, 0, 3]]
-let D:number[][]= [[2,1,4], [0,1,1]]
-let E:number[][]= [[6, 3, -1, 0], [1, 1, 0, 4], [-2, 5, 0, 2]]
-let C:number[][]= multiply(A,B)
-let F:number[][]= multiply(D,E)
-let G:number = F[0][0]
-let H:number[][]= [[1,3,4],[0,2,3]]
-let I:number[][]= [[4,1,2],[0,2,0],[1,0,2]]
-
-console.log(minor(I,1,1))
-
-console.log(A)
-console.log(size(A))
-console.log(C)
-console.log(size(C))
-console.log(F)
-console.log(size(F))
-console.log(G)
-console.log(transpose(H))
